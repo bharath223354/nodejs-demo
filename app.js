a97b650e778dc719e6bce3ea9ba62761489b7f10
Bharath Raj JV <bharathrajjv@Bharaths-MacBook-Air.local>
2026-09-25 12:52:11 +0530

    added console in app js

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello from Node.js application!");
+  res.send("Hello from Node.js application!123");
 });
 
 app.listen(PORT, () => {
+    console.log("hello")
   console.log(`Server running on port ${PORT}`);
 });
