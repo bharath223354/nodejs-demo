7a9858e9521faf7ac7e8f0bcecdc978a2260cb01
Bharath Raj JV <bharathrajjv@Bharaths-MacBook-Air.local>
2026-09-25 13:25:41 +0530

    added console -45

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello from Node.js application!123-4");
+  res.send("Hello from Node.js application!123-45");
 });
 
 app.listen(PORT, () => {
-    console.log("hello-4")
+    console.log("hello-45")
   console.log(`Server running on port ${PORT}`);
 });
